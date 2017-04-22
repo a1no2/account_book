2035460b10ff3b885d899a769eb94646353ba6f7
kenji <a1no2jz2l3@gmail.com>
2017-04-22 08:41:15 +0000

    first commit

---
+++ app.rb
@@ -0,0 +1,30 @@
+require 'bundler/setup'
+Bundler.require
+require 'sinatra/reloader' if development?
+
+require './models/item.rb'
+
+get '/' do
+    @items = Item.all
+    @total = Item.sum(:price)
+    @categories = Category.all
+    erb :index
+end
+
+post '/create' do
+   Item.create({
+       title: params[:title],
+       price: params[:price],
+       category_id: params[:category]
+    }) 
+    
+redirect '/'
+end
+
+get '/category/:id' do
+    @categories = Category.all
+    @category = Category.find(params[:id])
+    @category_name = @category.name
+    @items = @category.items
+    erb :index
+end
